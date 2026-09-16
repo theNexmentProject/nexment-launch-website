05fc60c3403d46182fa9a86935a1d1523cae5e7a
ansh8996 <its.me.anshraj7194@gmail.com>
2026-09-16 09:42:43 +0530

    Hero Section and Final Touch

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,9 +1,20 @@
 import styles from "./hero.module.css"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Hero() {
   return (
-    <div>
-    
+    <div className={styles.main}>
+      <div className={styles.container} >
+        <Image src="/logo.png" alt="The Nexment Project" height={50} width={150} /> 
+        <h1 className={styles.heading} >The Nexment Project</h1>
+        <h3>Building simple, open-source tools for developers, creators, and builders.</h3>
+        <div className={styles.button} >
+          <Link className={styles.primaryBtn} href="https://github.com/theNexmentProject/nexment-cli"><i className="fa-solid fa-terminal"></i> Explore CLI</Link>
+          <Link className={styles.secondaryBtn} href="https://github.com/theNexmentProject"><i className="fa-brands fa-github"></i> Github Organisation</Link>
+        </div>
+        <h2>Launching • 11 November 2026</h2>
+      </div>
     </div>
   )
 }
